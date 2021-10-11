c196fbd89e75d1d74858a8c83c915c2f661b74d8
Ubuntu <ubuntu@ip-172-31-31-203.ec2.internal>
2021-10-11 15:58:09 +0000

    Add data loader for CNNDM data and Bert large uncased model for pytorch

--- qmsum/qmsum_master/data_loader.py
+++ qmsum/qmsum_master/data_loader.py
@@ -1,11 +1,16 @@
 import logging
 import os
 import zipfile
+from zipfile import ZipFile
+from urllib.request import urlopen
 
 log = logging.getLogger(__name__)
 
-path_to_cnndm_zip = "CNNDM/CNNDM.zip"
-path_to_check_cnndm_already_extracted = "CNNDM/train"
+path_to_tmp_zip = "../../qmsum_data/tmp.zip"
+extraction_path = "../../qmsum_data/CNNDM"
+url_to_cnndm_zip = "https://meeting-summarization-project.s3.amazonaws.com/CNNDM.zip"
+url_to_bert_large_uncased = "https://meeting-summarization-project.s3.amazonaws.com/pytorch_bert_large_uncased.bin"
+path_bert_large_uncased = "model/pytorch_bert_large_uncased.bin"
 
 def output(message):
     log.info(message)
@@ -15,18 +20,35 @@ def output(message):
 def main():
     '''
         If not already present, this function will process the following files:
-        a) Unzipping CNNDM.zip in folder CNNDM
+        a) Unzipping CNNDM.zip in folder qmsum_data/CNNDM
+        b) Downloading BERT large uncased model and saving it in folder model in qmsum_master
         
     '''
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    if os.path.exists(path_to_check_cnndm_already_extracted):
-        output("CNNDM already extracted. Skipping this step")
+    if os.path.exists("../../qmsum_data/CNNDM"):
+        output("CNNDM data already extracted. Skipping this step")
     else:
-        output("Starting to extract CNNDM.zip. This can take some minutes")
-        with zipfile.ZipFile(path_to_cnndm_zip, 'r') as zip_ref:
-            zip_ref.extractall(current_dir)
+        output("Starting to download CNNDM.zip. This can take some minutes.")
+        zipresp = urlopen(url_to_cnndm_zip)
+        tempzip = open(path_to_tmp_zip, "wb")
+        tempzip.write(zipresp.read())
+        tempzip.close()
+        zf = ZipFile(path_to_tmp_zip)
+        zf.extractall(path=extraction_path)
+        zf.close()
         output("CNNDM extracted")
     
+    if os.path.exists(path_bert_large_uncased):
+        output("Bert uncased large model already in subfolder model. Skipping this step")
+    else:
+        output("Downloading Bert large uncased model for pytorch. This can take some minutes.")
+        bert_model = urlopen(url_to_bert_large_uncased)
+        bert_output = open(path_bert_large_uncased, "wb")
+        bert_output.write(bert_model.read())
+        bert_output.close()
+        output("Bert large uncased for Pytorch written in model folder.")
+        
+        
+        
 
 if __name__ == "__main__":
     main()
